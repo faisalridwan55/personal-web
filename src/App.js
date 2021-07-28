621f19854a6599355e0df3593499648ca8ecf0ff
faisal ridwan <faisalridwan55@gmail.com>
2021-07-28 19:35:50 +0700

    Initiate front page and add Grid component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import styled from 'styled-components';
+import { BrowserRouter as Router } from "react-router-dom";
+import { VContainer } from './components/Grid';
+
 function App() {
   return (
     <div className="App">
+      <VContainer>
+        <div>
+          <h1>TESTING</h1>
+        </div>
+        <div>
+          <h1>TESTING</h1>
+        </div>
+      </VContainer>      
     </div>
   );
 }
---
+++ src/components/Grid.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const VContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const HContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
