54a84365786469f7bfb166978fff3bafccb7557d
Faisal Ridwan <faisalridwan55@gmail.com>
2022-10-14 12:47:29 +0700

    Change home content

--- src/containers/Home/components.js
+++ src/containers/Home/components.js
@@ -7,85 +7,88 @@ import { BLACK } from "../../constants/Colors";
 import IsDesktop from "../../functions/isDesktop";
 
 const timeFormatter = () => {
-    const time = new Date().toLocaleTimeString()
+  const time = new Date().toLocaleTimeString();
 
-    return `${time.slice(0,5)} ${time.slice(8)}`
-}
+  return `${time.slice(0, 5)} ${time.slice(8)}`;
+};
 
 export const Footer = () => {
-    const [time, setTime] = useState(timeFormatter())
+  const [time, setTime] = useState(timeFormatter());
 
-    useEffect(() => {
-        setTimeout(() => {
-            setTime(timeFormatter())
-        }, 60000)
-    }, [time])
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(timeFormatter());
+    }, 60000);
+  }, [time]);
 
-    return (
-        <Text 
-            style={{
-                bottom: 0,
-                right: 0,
-                left: 0,
-                padding: '16px',
-                position: 'absolute',
-                textAlign: 'center'
-            }}>
-            {`It's ${time} now at my place (Jakarta)`}
-        </Text>
-    )
-}
+  return (
+    <Text
+      style={{
+        bottom: 0,
+        right: 0,
+        left: 0,
+        padding: "16px",
+        position: "absolute",
+        textAlign: "center",
+      }}
+    >
+      {`It's ${time} now at my place (Jakarta)`}
+    </Text>
+  );
+};
 
 export const SecretText = () => {
-    const [revealSecret, setRevealSecret] = useState(false)
-    const isDesktop = IsDesktop()
-    const revealVariants = {
-        visible: { opacity: 1},
-        hidden: { opacity: 0}
-    }
-    const secretVariants = {
-        visible: { opacity: 1, y: isDesktop ? -30 : -20},
-        hidden: { opacity: 0}
-    }
+  const [revealSecret, setRevealSecret] = useState(false);
+  const isDesktop = IsDesktop();
+  const revealVariants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  };
+  const secretVariants = {
+    visible: { opacity: 1, y: isDesktop ? -30 : -20 },
+    hidden: { opacity: 0 },
+  };
 
-    return (
-        <Box 
-            onMouseLeave={() => setRevealSecret(false)}
-            minHeight='100px'
-            marginTop='8px'
-            alignItems='center'
+  return (
+    <Box
+      onMouseLeave={() => setRevealSecret(false)}
+      minHeight="100px"
+      marginTop="8px"
+      alignItems="center"
+    >
+      <AnimatePresence>
+        <Text
+          as={motion.i}
+          size="l"
+          color={BLACK}
+          textAlign="center"
+          width="fit-content"
+          whileHover={{
+            scale: 1.2,
+            textShadow: "0px 0px 10px rgba(255, 255, 255, 1)",
+            cursor: "pointer",
+          }}
+          onClick={() => setRevealSecret(true)}
+          animate={!revealSecret ? "visible" : "hidden"}
+          variants={revealVariants}
+          transition={{ duration: 1 }}
         >
-            <AnimatePresence>
-                <Text as={motion.i}
-                    size="l"
-                    color={BLACK}
-                    textAlign="center"
-                    width='fit-content'
-                    whileHover={{
-                        scale: 1.2,
-                        textShadow: '0px 0px 10px rgba(255, 255, 255, 1)', 
-                        cursor: 'pointer'
-                    }}
-                    onClick={() => setRevealSecret(true)}
-                    animate={!revealSecret ? 'visible' : 'hidden'}
-                    variants={revealVariants}
-                    transition={{duration: 1}}
-                >
-                    Click to reveal my secret
-                </Text>
-                {revealSecret && 
-                    <Text as={motion.i}
-                        size="l"
-                        color={BLACK}
-                        textAlign="center"
-                        animate={revealSecret ? 'visible' : 'hidden'}
-                        variants={secretVariants}
-                        transition={{duration: 1}}
-                    >
-                        I still use my spare time for coding
-                    </Text>
-                }
-            </AnimatePresence>
-        </Box>
-    )
-}
+          Click to reveal my secret
+        </Text>
+        {revealSecret && (
+          <Text
+            as={motion.i}
+            size="l"
+            color={BLACK}
+            textAlign="center"
+            animate={revealSecret ? "visible" : "hidden"}
+            variants={secretVariants}
+            transition={{ duration: 1 }}
+          >
+            I have an experience to be Project Manager from previous company
+          </Text>
+        )}
+      </AnimatePresence>
+    </Box>
+  );
+};
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -7,42 +7,46 @@ import PageContainer from "../../components/PageContainer";
 import CollapsibleRow from "../../components/CollapsibleRow";
 import { FONT_SIZE } from "../../constants/Size";
 
-const Home = () => ( 
-    <PageContainer 
-        alignItems="center"
-        justifyContent="center"
-        className="home-container"
+const Home = () => (
+  <PageContainer
+    alignItems="center"
+    justifyContent="center"
+    className="home-container"
+  >
+    <Text size="xxl">Hi!</Text>
+    <Title
+      style={{ fontSize: FONT_SIZE().title, zIndex: "997" }}
+      textAlign="center"
+      transition={{ duration: 1 }}
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
     >
-        <Text size="xxl">Hi!</Text>
-        <Title 
-            style={{fontSize: FONT_SIZE().title, zIndex:'997'}}
-            textAlign='center'
-            transition={{duration: 1}}
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-        >
-            I'm Faisal Ridwan
-        </Title>
-        <Box>
-            <Text size="xl" color={BLACK} textAlign="center" bold>
-                Currently, I responsible to manage the technical aspect of the project
-                at the company that provides IT solutions for Indonesian Military organization
-            </Text>
-            <SecretText />
-            <CollapsibleRow>
-                <li>
-                    This is a homemade website made by React Framework
-                    <a href="https://github.com/faisalridwan55/personal-web" target="_blank" rel="noopener noreferrer">
-                        (Repo link)
-                    </a>
-                </li>
-                <li>
-                    This website is still on development, update frequently when I have free time
-                </li>
-            </CollapsibleRow>
-        </Box>
-        <Footer />
-    </PageContainer>
+      I'm Faisal Ridwan
+    </Title>
+    <Box>
+      <Text size="xl" color={BLACK} textAlign="center" bold>
+        Currently, I work as full-stack developer for company from Singapore.
+      </Text>
+      <SecretText />
+      <CollapsibleRow title="This is collapsible header (click me!)">
+        <li style={{ lineHeight: "32px" }}>
+          {`This is a homemade website made by React Framework `}
+          <a
+            href="https://github.com/faisalridwan55/personal-web"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            (Repo link)
+          </a>
+        </li>
+        <li>
+          This website is still on development, update frequently when I have
+          free time
+        </li>
+      </CollapsibleRow>
+    </Box>
+    <Footer />
+  </PageContainer>
 );
- 
+
 export default Home;
