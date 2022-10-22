04b731e4a0e1bfb6683f0763ec377a1d2a0454d4
Faisal Ridwan <faisalridwan55@gmail.com>
2022-10-23 01:33:28 +0700

    Adjust font family

--- src/components/Text.js
+++ src/components/Text.js
@@ -4,18 +4,17 @@ import { WHITE } from "../constants/Colors";
 import { FONT_SIZE } from "../constants/Size";
 
 export const Title = styled(motion.p)`
-    margin: 0;
-    font-weight: bold;
-    color: ${props => props.color || WHITE};
-    text-align: ${props => props.textAlign};
-    font-family: Zilla Slab, serif;
-    ${props => `font-size: ${FONT_SIZE()[props.size || 'xxxl']};`}
+  margin: 0;
+  color: ${(props) => props.color || WHITE};
+  text-align: ${(props) => props.textAlign};
+  font-family: "Silkscreen", cursive;
+  ${(props) => `font-size: ${FONT_SIZE()[props.size || "xxxl"]};`}
 `;
 
 export const Text = styled.text`
-    width: ${props => props.width};
-    text-align: ${props => props.textAlign};
-    color: ${props => props.color || WHITE};
-    ${props => props.bold && `font-weight: bold;`}
-    ${props => `font-size: ${FONT_SIZE()[props.size || 'md']};`}
+  width: ${(props) => props.width};
+  text-align: ${(props) => props.textAlign};
+  color: ${(props) => props.color || WHITE};
+  ${(props) => props.bold && `font-weight: bold;`}
+  ${(props) => `font-size: ${FONT_SIZE()[props.size || "md"]};`}
 `;
